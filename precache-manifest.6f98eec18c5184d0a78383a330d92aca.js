fe3c2b2879abbbfa3d7b8ab3f65d1ff4afc216a6
Prethiv Nageswaran <prethiv191@gmail.com>
2020-05-10 12:24:22 +0530

    Updates

---
+++ precache-manifest.6f98eec18c5184d0a78383a330d92aca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7c84ac207f55a10030aca43eb4eaf3",
+    "url": "/postonline/index.html"
+  },
+  {
+    "revision": "ba0424caf682a41ec9c1",
+    "url": "/postonline/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "03b7b1c86f9e22c6ee65",
+    "url": "/postonline/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba0424caf682a41ec9c1",
+    "url": "/postonline/static/js/2.34e50f83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/postonline/static/js/2.34e50f83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b7b1c86f9e22c6ee65",
+    "url": "/postonline/static/js/main.0532cc31.chunk.js"
+  },
+  {
+    "revision": "c1ee39a398aa5338a839",
+    "url": "/postonline/static/js/runtime-main.f8136752.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/postonline/static/media/logo.5d5d9eef.svg"
+  }
+]);
